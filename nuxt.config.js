ee87c4e2e53ea85740e867a586840471cece47bc
prismistim <prismistim@hotmail.com>
2020-02-16 14:01:46 +0900

    ogとfaviconの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@Prismist_M' },
+      { hid: 'og:url', property: 'og:url', content: 'https://tokiwa.netlify.com' },
+      { hid: 'og:title', property: 'og:title', content: 'tokiwa' },
+      { hid: 'og:description', property: 'og:description', content: 'IIDX Option Viewer' },
+      { hid: 'og:image', property: 'og:image', content: 'https://tokiwa.netlify.com/twitter.png' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'tokiwa'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
